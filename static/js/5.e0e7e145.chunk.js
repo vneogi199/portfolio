dbf52f48f3dd292c2cad255b9cd0da69c703bb66
Vinit Neogi <vneogi199@gmail.com>
2019-09-09 21:28:54 +0530

    Updates

---
+++ static/js/5.e0e7e145.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpportfolio=window.webpackJsonpportfolio||[]).push([[5],{83:function(t,e,n){"use strict";n.r(e);var a=n(16),o=n(8),c=n(17),i=n(18),r=n(19),u=n(0),p=n.n(u),s=function(t){function e(){return Object(a.a)(this,e),Object(c.a)(this,Object(i.a)(e).apply(this,arguments))}return Object(r.a)(e,t),Object(o.a)(e,[{key:"render",value:function(){return p.a.createElement("div",{className:"m-0 h-100 bg-light"})}}]),e}(p.a.Component);e.default=s}}]);
+//# sourceMappingURL=5.e0e7e145.chunk.js.map
