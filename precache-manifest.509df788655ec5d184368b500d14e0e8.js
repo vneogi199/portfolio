917343a8bf6d1e83909e5d94258b561509ddd291
Vinit Neogi <vneogi199@gmail.com>
2019-09-09 21:36:53 +0530

    Updates

---
+++ precache-manifest.509df788655ec5d184368b500d14e0e8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b24abbe9560f8665e0eb90d6412fc38",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c8cfe62a8848beaf8ce2",
+    "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2ca3a2a98c84003991db",
+    "url": "/portfolio/static/css/main.30d1c854.chunk.css"
+  },
+  {
+    "revision": "c8cfe62a8848beaf8ce2",
+    "url": "/portfolio/static/js/2.0e8332a0.chunk.js"
+  },
+  {
+    "revision": "520f5830d883b9163792",
+    "url": "/portfolio/static/js/3.e3efa343.chunk.js"
+  },
+  {
+    "revision": "c27d152682abf04082f0",
+    "url": "/portfolio/static/js/4.b1cc47da.chunk.js"
+  },
+  {
+    "revision": "d5699cc479a86fa5c8f4",
+    "url": "/portfolio/static/js/5.e0e7e145.chunk.js"
+  },
+  {
+    "revision": "20a8f5bf2fb4924f0f75",
+    "url": "/portfolio/static/js/6.b3f6ba2d.chunk.js"
+  },
+  {
+    "revision": "2ca3a2a98c84003991db",
+    "url": "/portfolio/static/js/main.b4072685.chunk.js"
+  },
+  {
+    "revision": "6363d82079376b309bb9",
+    "url": "/portfolio/static/js/runtime~main.3e1c72e4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5802fa2ad9dbe957d785c2c174526be.js"
+  "/portfolio/precache-manifest.509df788655ec5d184368b500d14e0e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
